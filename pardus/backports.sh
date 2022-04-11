e4bc73720d0125caa06e9575de43b520633f4994
sulincix <55052890+sulincix@users.noreply.github.com>
2022-04-11 15:02:22 +0000

    Delete backports.sh

--- pardus/backports.sh
+++
@@ -1,5 +0,0 @@
-echo "deb https://depo.pardus.org.tr/backports yirmibir-backports main contrib non-free" > /etc/apt/sources.list.d/yirmibir-backports.list
-apt update
-apt install -t yirmibir-backports linux-image-amd64 -yq
-apt autoremove -y
-apt clean
