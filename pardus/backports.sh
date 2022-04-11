6cf9c873e26bcac06b826f9a8e791cd0479443bc
sulincix <55052890+sulincix@users.noreply.github.com>
2022-04-11 14:58:06 +0000

    Update backports.sh

--- pardus/backports.sh
+++ pardus/backports.sh
@@ -1,4 +1,5 @@
 echo "deb https://depo.pardus.org.tr/backports yirmibir-backports main contrib non-free" > /etc/apt/sources.list.d/yirmibir-backports.list
 apt update
-apt -t yirmibir-backports full-upgrade -y
+apt install -t yirmibir-backports linux-image-amd64 -yq
 apt autoremove -y
+apt clean
