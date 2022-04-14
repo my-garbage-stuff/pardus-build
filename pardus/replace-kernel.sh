1a6f4c4263a24dd0adb726ad3d94b25c98717274
sulincix <55052890+sulincix@users.noreply.github.com>
2022-04-14 09:57:39 +0000

    Update replace-kernel.sh

--- pardus/replace-kernel.sh
+++ pardus/replace-kernel.sh
@@ -1,5 +1,5 @@
-link=$(wget -O - https://github.com/debian-kernel/linux/releases | grep "amd64.deb" | sed "s/.*href=\"//g;s/\".*//g")
-wget $link -o linux.deb
+link=$(wget -O - https://github.com/debian-kernel/linux/releases | grep "href" | grep "amd64.deb" | sed "s/.*href=\"//g;s/\".*//g")
+wget "https://github.com/$link" -O linux.deb
 apt purge linux-image-amd64 -y
 apt autoremove -y
 dpkg -i linux.deb
