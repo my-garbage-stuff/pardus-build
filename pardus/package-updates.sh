0c2af674e39a452003fa1b0b3f5110d08f958baa
sulincix <55052890+sulincix@users.noreply.github.com>
2022-05-15 16:58:06 +0000

    Update package-updates.sh

--- pardus/package-updates.sh
+++ pardus/package-updates.sh
@@ -4,9 +4,6 @@ apt full-upgrade -y
 apt purge libreoffice-common -y
 apt install libreoffice-latest -y
 apt purge firefox-esr -y
-mkdir -p /debs
-wget https://github.com/app-installer/firefox/raw/master/firefox-installer.deb -O /debs/firefox.deb
-dpkg -i /debs/*.deb
+apt install librewolf -y
 apt install -y
 apt autoremove -y
-rm -rf /debs
