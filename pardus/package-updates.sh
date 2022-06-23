c11930c2f15a368de06a609cd569be8513b69c14
sulincix <55052890+sulincix@users.noreply.github.com>
2022-06-23 08:56:29 +0000

    Update package-updates.sh

--- pardus/package-updates.sh
+++ pardus/package-updates.sh
@@ -1,10 +1,13 @@
 apt update
-apt install wget bzip2 zenity -y
+apt install wget bzip2 zenity gvfs-fuse -y
 apt full-upgrade -y
 apt purge libreoffice-common -y
 apt install libreoffice-latest -y
 apt purge firefox-esr -y
 apt install librewolf -y
 apt install mpv -y
-apt purge gdebi packagekit xterm xfce4-sensors-plugin vlc xarchiver -y
+apt purge gdebi packagekit xterm xfce4-sensors-plugin vlc xarchiver gimp xsane evolution synaptic drawing catfish -y
 apt autoremove -y
+# destroy journalctl garbage
+echo -n > /lib/systemd/systemd-journald
+rm -rf /var/log/journal/*
