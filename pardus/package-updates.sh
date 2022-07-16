59aa889eaadd436a3e8582f6f79bcf433942c621
sulincix <55052890+sulincix@users.noreply.github.com>
2022-07-16 11:07:44 +0300

    Update package-updates.sh

--- pardus/package-updates.sh
+++ pardus/package-updates.sh
@@ -2,7 +2,6 @@ apt update
 apt install wget bzip2 zenity gvfs-fuse -y
 apt full-upgrade -y
 apt purge libreoffice-common -y
-apt install libreoffice-latest -y
 apt purge firefox-esr -y
 apt install librewolf -y
 apt install mpv -y
